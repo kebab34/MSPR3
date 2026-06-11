62ed231ac2739970999dba21bea7770a3a291890
kebab34 <ozturknatael@gmail.com>
2026-06-11 13:35:30 +0200

    ajout vrai valeur env

--- mobile/src/config/supabase.ts
+++ mobile/src/config/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const SUPABASE_URL = 'https://VOTRE_URL.supabase.co';
-const SUPABASE_ANON_KEY = 'VOTRE_ANON_KEY';
+const SUPABASE_URL = 'https://jgtnjeoaeoderkbzifyd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpndG5qZW9hZW9kZXJrYnppZnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMTQ2NjIsImV4cCI6MjA5Mjc5MDY2Mn0.TpWaVLYpoqfVXM-sGeDcvcFS5Nd-3ohUVbj3vJIO0DI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
