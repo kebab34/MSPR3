4ad1d56ae825de9da861df1b5f2ca00d90a41ac8
kebab34 <ozturknatael@gmail.com>
2026-06-24 11:31:40 +0200

    fix: request camera and media library permissions on app launch

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import Navigation from './src/navigation';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS !== 'web') {
+      ImagePicker.requestCameraPermissionsAsync();
+      ImagePicker.requestMediaLibraryPermissionsAsync();
+    }
+  }, []);
+
   return <Navigation />;
 }
